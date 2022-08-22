28982cc1dcd01eb489d5cbb5cdb9830538e2723a
q2333gh <1253102853@qq.com>
2022-08-22 19:41:48 +0800

    1

---
+++ Vector/Vector.h
@@ -0,0 +1,141 @@
+//
+// Created by 白桃乌龙 on 2022/8/22.
+//
+#include <iostream>
+
+#ifndef DSA_IMPL_VECTOR_H
+#define DSA_IMPL_VECTOR_H
+
+using Rank = int;
+#define DEFAULT_CAPACITY 8
+
+template<typename T>
+struct Increase {
+    virtual void operator()(T &e) { e++; }
+};
+
+template<typename T>
+class Vector {
+protected:
+    Rank _size;
+    int _capacity;
+    T *_elem;
+
+public:
+
+    explicit //c,capacity ; s,size ; v,elem
+    Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0) {
+        _elem = new T[_capacity = c];
+        for (_size = 0; _size < s; _elem[_size++] = v);
+    }
+
+    Rank size() { return _size; }
+
+
+    void expand() {
+        if (_size < _capacity) return;
+        if (_capacity < DEFAULT_CAPACITY) _capacity = DEFAULT_CAPACITY;
+        T *oldElem = _elem;
+        _elem = new T[_capacity <<= 1];
+        for (int i = 0; i < _size; i++) _elem[i] = oldElem[i];
+        std::cout << "expand to : " << _size << " ";
+        delete[] oldElem;
+    }
+
+    Rank insert(Rank r, T const &e) {
+        expand();
+        for (int i = _size; i > r; i--) {
+            _elem[i] = _elem[i - 1];
+        }
+        _elem[r] = e;
+        _size++;
+        return r;
+    }
+
+    int remove(Rank lo, Rank hi) {
+        if (lo == hi) return 0;
+        while (hi < _size)_elem[lo++] = _elem[hi++];
+        _size = lo;
+        return hi - lo;
+    }
+
+    T remove(Rank r) {//单元素删除重载
+        T e = _elem[r];
+        remove(r, r + 1);
+        return e;
+    }
+
+    void traverse(void (*visit)(T &)) {//*函数指针
+        for (int i = 0; i < _size; i++) {
+            visit(_elem[i]);
+        }
+    }
+
+    void increase(Vector<T> &V) {
+        V.traverse(Increase<T>());
+    }
+
+
+    T &operator[](Rank r) { return _elem[r]; }
+
+    void printV() {
+        for (int i = 0; i < _size; ++i) {
+            std::cout << _elem[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    Rank find(T const &e, Rank lo, Rank hi) {
+        while (lo < hi) {
+            if (e == _elem[hi]) return hi;
+            else
+                hi--;
+        }
+        return -1;
+    }
+
+    int disordered() const {
+        int n = 0;
+        for (int i = 1; i < _size; i++) {
+            if (_elem[i - 1] > _elem[i]) {
+                n++;
+            }
+        }
+        return n;
+    }
+
+    void bubbleSort(Rank lo, Rank hi) {
+        for (Rank last = --hi; lo < hi; hi = last)
+            for (Rank i = last = lo; i < hi; i++) {
+                swap(_elem[last = i], _elem[i + 1]);
+            }
+    }
+
+    Rank maxItem(Rank lo, Rank hi) {
+        Rank mx = hi;
+        while (lo < hi--) {
+            if (_elem[hi] > _elem[mx])
+                mx = hi;
+        }
+        return mx;
+    }
+
+    void swap(T &a, T &b) {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    void selecionSort(Rank lo, Rank hi) {
+        while (lo < hi--) {
+            swap(_elem[maxItem(lo, hi)], _elem[hi]);
+        }
+    }
+
+//    int deduplicate(){
+//        int oldSize=_size;
+//        Rank i =
+//    }
+};
+
+#endif //DSA_IMPL_VECTOR_H
---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Vector/Vector.h"
+int main() {
+    srand((unsigned)time(nullptr));
+    std::cout << "Hello, Vector!" << std::endl;
+    Vector<int> v1(0,0,0);
+    for (int i = 0; i < v1.size(); ++i) {
+        std::cout<<v1[i]<<"\t";
+    }
+    std::cout<<std::endl;
+    for (int i = 0; i < 10; i++) {
+        v1.insert(0, rand()%10);
+    }
+    std::cout<<std::endl;
+    std::cout<<"size:"<<v1.size();
+    std::cout<<std::endl;
+    for (int i = 0; i < v1.size(); ++i) {
+        std::cout<<v1[i]<<"\t";
+    }
+    std::cout<<std::endl;
+    v1.remove(0,3);
+    for (int i = 0; i < v1.size(); ++i) {
+        std::cout<<v1[i]<<"\t";
+    }
+    std::cout<<std::endl;
+    std::cout<<"remove pos 1 : " <<v1.remove(0)<<std::endl;
+    std::cout<<"remove pos 2 : " <<v1.remove(0)<<std::endl<<"new v1: ";
+    v1.printV();
+    std::cout<<"try find 0,pos(max rank) : "<<v1.find(0,0,4)<<"  get via pos: "<<v1[v1.find(0,0,4)]<<std::endl;
+//    v1.increase(v1);
+//    std::cout<<"+1 v1:";v1.printV();
+    std::cout<<"disoredred n: "<<v1.disordered();
+//    v1.bubbleSort(0,v1.size());
+//    std::cout<<"bubbleSort n1: ";
+    v1.selecionSort(0,v1.size());
+    v1.printV();
+
+
+    return 0;
+}
